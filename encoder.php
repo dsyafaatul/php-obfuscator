644ddeb0ef202a80eb62b1b31a66cfd7deb2c0fd
D. Syafa'atul Anbiya <dikisyafaatul@gmail.com>
2022-03-22 11:14:24 +0700

    Fix include/require recursively encoded files

--- encoder.php
+++ encoder.php
@@ -1,22 +1,20 @@
 <?php
 ini_set('display_errors', 1);
-function encode($in = './input/', $out= './output/', $word_list = []){
+function encode($in = './input/', $out= './output/', $word_list = [], $exclude = [], $prefix = "dsyafaatul"){
     $input = realpath($in).DIRECTORY_SEPARATOR;
     $output = realpath($out).DIRECTORY_SEPARATOR;
 
     $algos = hash_algos();
-    if(($path = realpath($in)) && is_dir($path)){
-	$path .= DIRECTORY_SEPARATOR;
+    if($path = realpath($in).DIRECTORY_SEPARATOR){
         foreach(scandir($path) as $key => $filename){
-            if(in_array($filename, [".", ".."])) continue;
-            if(is_dir($path.$filename)) encode($path.$filename, $out, $word_list);
+            if(in_array($filename, array_merge([".", ".."], $exclude))) continue;
+            if(is_dir($path.$filename)) encode($path.$filename, $out, $word_list, $exclude);
             if(file_exists($path.$filename) && pathinfo($path.$filename, PATHINFO_EXTENSION) == 'php'){
                 $file = file($path.$filename);
                 $file_implode = implode('', $file);
 
                 $continue = TRUE;
                 while($continue){
-                    $prefix = "dsyafaatul";
                     $varname = $prefix.substr(str_shuffle('abcdefghijklmnopqrstuvwxyz0123456789'), 0, 8);
                     $funname = $prefix.substr(str_shuffle('abcdefghijklmnopqrstuvwxyz0123456789'), 0, 8);
                     $funname2 = $prefix.substr(str_shuffle('abcdefghijklmnopqrstuvwxyz0123456789'), 0, 8);
@@ -27,7 +25,7 @@ function encode($in = './input/', $out= './output/', $word_list = []){
 
                     $word = count($word_list) < 1 ? '' : $word_list[rand(0, count($word_list) -1)];
                     $algo = $algos[rand(0, count($algos)-1)];
-                    $key = count($word_list) < 1 ? '' : "<?php /* $word */ ";
+                    $key = count($word_list) < 1 ? '<?php ' : "<?php /* $word */ ";
 
                     $a = '$'.$varname.'=file(preg_replace("@\(.*\(.*$@","",__FILE__));
                     if(preg_replace("@\(.*\(.*$@","",__FILE__)==__FILE__ or
@@ -39,15 +37,15 @@ function encode($in = './input/', $out= './output/', $word_list = []){
 
                     $d_ = 'eval('.$funname2.'('.$funname.'($'.$varname.'[0],'.$angka3.'),'.$funname.'($'.$varname.'[0],'.$angka1.'),$'.$varname.'));__halt_compiler();';
 
-                    $b = 'function '.$funname.'($a,$b){$c=array('.(strlen($key)+strlen($a_)+strlen($c_)+strlen($d_)+strlen($rand1)).','.strlen($c).',32,266);if($b=='.$angka1.'){$d=substr($a,$c[0]+$c[1],$c[2]);}elseif($b=='.$angka2.'){$d=substr($a,$c[0],$c[1]);}elseif($b=='.$angka3.'){$d=trim(substr($a,$c[0]+$c[1]+$c[2]));}return$d;}';
+                    $b = 'if(!function_exists("'.$funname.'")){function '.$funname.'($a,$b){$c=array('.(strlen($key)+strlen($a_)+strlen($c_)+strlen($d_)+strlen($rand1)).','.strlen($c).',32,266);if($b=='.$angka1.'){$d=substr($a,$c[0]+$c[1],$c[2]);}elseif($b=='.$angka2.'){$d=substr($a,$c[0],$c[1]);}elseif($b=='.$angka3.'){$d=trim(substr($a,$c[0]+$c[1]+$c[2]));}return$d;}}';
                     $b_ = "eval(base64_decode('".base64_encode($b)."'));";
 
-                    $b = 'function '.$funname.'($a,$b){$c=array('.(strlen($key)+strlen($a_)+strlen($b_)+strlen($c_)+strlen($d_)+strlen($rand1)).','.strlen($c).',32,266);if($b=='.$angka1.'){$d=substr($a,$c[0]+$c[1],$c[2]);}elseif($b=='.$angka2.'){$d=substr($a,$c[0],$c[1]);}elseif($b=='.$angka3.'){$d=trim(substr($a,$c[0]+$c[1]+$c[2]));}return$d;}';
+                    $b = 'if(!function_exists("'.$funname.'")){function '.$funname.'($a,$b){$c=array('.(strlen($key)+strlen($a_)+strlen($b_)+strlen($c_)+strlen($d_)+strlen($rand1)).','.strlen($c).',32,266);if($b=='.$angka1.'){$d=substr($a,$c[0]+$c[1],$c[2]);}elseif($b=='.$angka2.'){$d=substr($a,$c[0],$c[1]);}elseif($b=='.$angka3.'){$d=trim(substr($a,$c[0]+$c[1]+$c[2]));}return$d;}}';
                     $b_ = "eval(base64_decode('".base64_encode($b)."'));";
                     
                     $hash = $hash1 = hash($algo, $key.$a_.$b_.$c_.preg_replace('/__halt_compiler.*/', '', $d_));
 
-                    $b = 'function '.$funname.'($a,$b){$c=array('.(strlen($key)+strlen($a_)+strlen($b_)+strlen($c_)+strlen($d_)+strlen($rand1)).','.strlen($c).','.strlen($hash).',266);if($b=='.$angka1.'){$d=substr($a,$c[0]+$c[1],$c[2]);}elseif($b=='.$angka2.'){$d=substr($a,$c[0],$c[1]);}elseif($b=='.$angka3.'){$d=trim(substr($a,$c[0]+$c[1]+$c[2]));}return$d;}';
+                    $b = 'if(!function_exists("'.$funname.'")){function '.$funname.'($a,$b){$c=array('.(strlen($key)+strlen($a_)+strlen($b_)+strlen($c_)+strlen($d_)+strlen($rand1)).','.strlen($c).','.strlen($hash).',266);if($b=='.$angka1.'){$d=substr($a,$c[0]+$c[1],$c[2]);}elseif($b=='.$angka2.'){$d=substr($a,$c[0],$c[1]);}elseif($b=='.$angka3.'){$d=trim(substr($a,$c[0]+$c[1]+$c[2]));}return$d;}}';
                     $b_ = "eval(base64_decode('".base64_encode($b)."'));";
 
                     $hash = $hash2 = hash($algo, $key.$a_.$b_.$c_.preg_replace('/__halt_compiler.*/', '', $d_));
